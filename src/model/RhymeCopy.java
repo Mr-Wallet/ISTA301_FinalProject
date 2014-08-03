fe392c48551698af7b721f9125ff4e2c729af443
Jordan_Wallet <jjwallet@email.arizona.edu>
2014-08-03 13:43:22 -0700

    Added empty classes

--- src/model/RhymeCopy.java
+++ src/model/RhymeCopy.java
@@ -1,16 +1,20 @@
 package model;
 
+/**
+ * RhymeCopy uses a corpus of annotated rhyming lyrics to generate its own lyrics in the same style.
+ * Created as a final project for ISTA 301, Summer 2014.
+ * 
+ * @author Nicolas Azevedo Costa and Jordan Wallet
+ *
+ */
+
 public class RhymeCopy {
 
+	/**
+	 * 
+	 * @param args Expects a single argument, the directory to read lyric files from.
+	 */
 	public static void main(String[] args) {
-
-	// This is a comment
-
-		// TODO Auto-generated method stub
-		
-		//This is Jordan's comment
-		
-		//this is a new comment
 
 	}
 
