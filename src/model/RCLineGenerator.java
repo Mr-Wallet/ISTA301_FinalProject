3b3b45abdbdbb17355d914a5e435458e5eaf015b
Jordan_Wallet <jjwallet@email.arizona.edu>
2014-08-03 14:40:55 -0700

    Main method outline, RCLineGenerator start

--- src/model/RCLineGenerator.java
+++ src/model/RCLineGenerator.java
@@ -1,5 +1,35 @@
 package model;
 
-public class RCLineGenerator {
+import java.util.Random;
 
+/**
+ * Takes a word and then generates a line of appropriate length behind it.
+ */
+public class RCLineGenerator {
+	static RCLyricMap lyricMap;
+	static int defaultWordCount = 7;
+	
+	public static void setLyricMap(RCLyricMap map) {
+		lyricMap = map;
+	}
+	
+	public static String generateLine(String lastWord) {
+		int desiredWordCount = defaultWordCount - 1 + (new Random()).nextInt(2);
+		return generateLine(lastWord, desiredWordCount);
+	}
+	
+	public static String generateLine(String lastWord, int desiredWordCount) {
+		if(lyricMap == null) {
+			throw new NullPointerException("You forgot to set RCLineGenerator's lyric map");
+		}
+		String result = lastWord;
+		while(wordCount(result) < desiredWordCount) {
+			//TODO ask lyricmap for another word
+		}
+		return result;
+	}
+	
+	private static int wordCount(String s) {
+		return s.isEmpty() ? 0 : s.split("\\s+").length;
+	}
 }
--- src/model/RhymeCopy.java
+++ src/model/RhymeCopy.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.io.File;
+
 /**
  * RhymeCopy uses a corpus of annotated rhyming lyrics to generate its own lyrics in the same style.
  * Created as a final project for ISTA 301, Summer 2014.
@@ -15,7 +17,33 @@ public class RhymeCopy {
 	 * @param args Expects a single argument, the directory to read lyric files from.
 	 */
 	public static void main(String[] args) {
+		if(!validDirectory(args)) {
+			System.out.println("The argument should be a directory with annotated lyric files.");
+			System.exit(1);
+		}
 
+		RCRhymeDictionary rhymeDict = new RCRhymeDictionary();
+		RCLyricMap lyricMap = new RCLyricMap();
+		RCSongStructure songStruct = new RCSongStructure();
+		
+		//TODO iterate over the files in the directory and have RCFileReader handle them all
+		
+		//TODO generate a song structure
+		
+		RCLineGenerator.setLyricMap(lyricMap);
+		//TODO generate all the necessary lines based on the song structure
+		
+		//TODO use RCFileWriter to write the final song to an output file
 	}
 
+	private static boolean validDirectory(String[] args) {
+		if(args.length < 1 || args[0].length() < 1)
+			return false;
+		
+		File dir = new File(args[0]);
+		if(dir.isDirectory())
+			return false;
+
+		return true;
+	}
 }
