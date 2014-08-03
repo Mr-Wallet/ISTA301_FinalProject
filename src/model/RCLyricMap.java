e8c25998cc8c6fa830e182584b707fad60813ba6
acnicolas <nicolasazev@gmail.com>
2014-08-03 15:21:28 -0700

    // updating

--- src/model/RCLyricMap.java
+++ src/model/RCLyricMap.java
@@ -1,5 +1,12 @@
 package model;
 
+import java.util.HashMap;
+import java.util.TreeSet;
+
 public class RCLyricMap {
+	
+	private int textLength;
+	
+	private HashMap<String, TreeSet<String>> textMap;
 
 }
