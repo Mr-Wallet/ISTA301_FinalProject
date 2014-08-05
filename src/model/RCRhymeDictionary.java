5b8970fa7a0771a4b2b8a42ea3850c30b7e4bf87
acnicolas <nicolasazev@gmail.com>
2014-08-05 15:58:08 -0700

    // updating the tests in RDTest

--- src/model/RCRhymeDictionary.java
+++ src/model/RCRhymeDictionary.java
@@ -9,74 +9,75 @@ public class RCRhymeDictionary {
 	private ArrayList<ArrayList<String>> rhymeList = new ArrayList<ArrayList<String>>();
 
 	public List<String> getRandomRhymeList() {
-		
+
 		List<String> randomList;
 		Random rnd = new Random();
-		
+
 		randomList = rhymeList.get(rnd.nextInt(rhymeList.size()));
-		
+
 		return randomList;
 	}
 
+	
+
 	public void addWords(ArrayList<String> arrayList) {
-		
+
 		boolean found = false;
 
 		if(rhymeList.isEmpty()){
 			rhymeList.add(arrayList);
+			return;
 		}
-		else{
+		
+		ArrayList<Integer> track = new ArrayList<Integer>();
 
-			// This nested for loop is used to merge all words of array List
-			for(int i=0 ; i < arrayList.size(); i++){ //for used to get each word of the parameter
-
-				ArrayList<Integer> track = new ArrayList<Integer>();
-
-				// for which takes each group of arrayList in rhymeList
-				for(int j=0; j < rhymeList.size(); j++){
-
-					// for which takes each word of arrayList in rhymeList
-					for(int k=0; k < rhymeList.get(j).size(); k++){
-
-						if(arrayList.get(i).equals(rhymeList.get(j).get(k))){
-
-							track.add(j);
-							break;
-
-						}
-
-					}
-				}
-
-				merge(track);
-				
-			}
-
-			// nested for loop which add the arrayList parameter if found the words
-			// in the rhymeList. Otherwise, creates a new group of arrayList in the
-			// rhymeList.
-			for(int i=0 ; i < arrayList.size() && !found; i++){ //for used to get each word of the parameter
-
-				// for which takes each group of arrayList in rhymeList
-				for(int j=0; j < rhymeList.size(); j++){
-
-					if(rhymeList.get(j).contains(arrayList.get(i))){
-						rhymeList.get(j).addAll(arrayList);
-						found = true;
-						break;
-					}
-					
-				}
-				
-			}
+		// This nested for loop is used to merge all words of array List
+		for(int i=0 ; i < arrayList.size(); i++){ //for used to get each word of the parameter		
 			
-			// if used to verify if the arrayList in the parameter was already added into the 
-			// rhymeList. If not, it will create a new group of rhymes.
-			if (found == false)
-				rhymeList.add(arrayList);
+			// for which takes each group of arrayList in rhymeList
+			for(int j=0; j < rhymeList.size(); j++){
+				// for which takes each word of arrayList in rhymeList
+				for(int k=0; k < rhymeList.get(j).size(); k++){
 
+					if(arrayList.get(i).equals(rhymeList.get(j).get(k))){
+
+						track.add(j);
+						break;
+
+					}
+
+				}
+			}
 
 		}
+
+		merge(track);
+
+		// nested for loop which add the arrayList parameter if found the words
+		// in the rhymeList. Otherwise, creates a new group of arrayList in the
+		// rhymeList.
+		for(int i=0 ; i < arrayList.size() && !found; i++){ //for used to get each word of the parameter
+
+			// for which takes each group of arrayList in rhymeList
+			for(int j=0; j < rhymeList.size(); j++){
+
+				if(rhymeList.get(j).contains(arrayList.get(i))){
+					rhymeList.get(j).addAll(arrayList);
+					found = true;
+					break;
+				}
+
+			}
+
+		}
+
+		// if used to verify if the arrayList in the parameter was already added into the 
+		// rhymeList. If not, it will create a new group of rhymes.
+		if (found == false)
+			rhymeList.add(arrayList);
+
+		
+
 	}
 
 	// Merge help method which merge the arrayLists in common in the rhymeList.  
@@ -85,20 +86,20 @@ public class RCRhymeDictionary {
 		for(int i=track.size()-1; i>0; i--){
 
 			rhymeList.get(track.get(0)).addAll(rhymeList.get(track.get(i)));
-			rhymeList.remove(track.get(i));			
+			rhymeList.remove(rhymeList.get(track.get(i)));			
 		}
 
 
 	}
-	
+
 	public int getSize(){
 		return rhymeList.size();
 	}
-	
+
 	public int getSizeGivenRhymeList(int index){
 		return rhymeList.get(index).size();
 	}
-	
+
 	public List<String> getRhymeList(int index) {
 
 		List<String> list;
@@ -107,6 +108,28 @@ public class RCRhymeDictionary {
 
 		return list;
 	}
+	
+	public String toString(){
+		
+		String str = "{ ";
+		for(int j=0; j < rhymeList.size(); j++){
+			
+			for(int k=0; k < rhymeList.get(j).size(); k++){
+				if(k+1 == rhymeList.get(j).size())
+					str += rhymeList.get(j).get(k);
+				else	
+				    str += rhymeList.get(j).get(k)+", ";
+			}
+			if(j+1 == rhymeList.size())
+				str+=" }\n";
+			else
+				str+=" }\n{ ";
+			
+		}
+		return str;
+	}
+
+
 
 
 
--- src/test/RCRhymeDictionaryTest.java
+++ src/test/RCRhymeDictionaryTest.java
@@ -29,11 +29,13 @@ public class RCRhymeDictionaryTest {
 		
 		assertEquals(1,rd.getSize());
 		assertEquals(3,rd.getSizeGivenRhymeList(0));
+		
+		System.out.println(rd.toString());
      	
 	}
 	
 	@Test
-	public void testTwoSetMerged() {
+	public void testTwoSet() {
 		RCRhymeDictionary rd = new RCRhymeDictionary();
 		
 		ArrayList<String> al = new ArrayList<String>();
@@ -52,16 +54,177 @@ public class RCRhymeDictionaryTest {
 		rd.addWords(al2);
 		
 		assertEquals(1,rd.getSize());
-	//	assertEquals(5,rd.getSizeGivenRhymeList(1));
+		assertEquals(6,rd.getSizeGivenRhymeList(0));
 		
-	//	assertTrue(rd.getRandomRhymeList().contains("blue"));
-	//	assertTrue(rd.getRandomRhymeList().contains("clue"));
-	//	assertTrue(rd.getRandomRhymeList().contains("due"));
-	//	assertTrue(rd.getRandomRhymeList().contains("shoe"));
-	//	assertTrue(rd.getRandomRhymeList().contains("drew"));
+		assertTrue(rd.getRandomRhymeList().contains("blue"));
+    	assertTrue(rd.getRandomRhymeList().contains("clue"));
+		assertTrue(rd.getRandomRhymeList().contains("due"));
+		assertTrue(rd.getRandomRhymeList().contains("shoe"));
+		assertTrue(rd.getRandomRhymeList().contains("drew"));
 		
-		
-     	
+		System.out.println(rd.toString());
 	}
+	
+	@Test
+	public void testTwoSetAndOneDifferent() {
+		RCRhymeDictionary rd = new RCRhymeDictionary();
+		
+		ArrayList<String> al = new ArrayList<String>();
+		
+		
+		al.add("blue");
+		al.add("clue");
+		al.add("due");
+		
+		rd.addWords(al);
+		
+		ArrayList<String> al2 = new ArrayList<String>();
+		
+		al2.add("shoe");
+		al2.add("drew");
+		al2.add("clue");
+		
+		rd.addWords(al2);
+		
+        ArrayList<String> al3 = new ArrayList<String>();
+		
+		al3.add("black");
+		al3.add("pac");
+		al3.add("lack");
+		
+		rd.addWords(al3);
+		
+		
+		assertEquals(2,rd.getSize());
+		assertEquals(6,rd.getSizeGivenRhymeList(0));
+		assertEquals(3,rd.getSizeGivenRhymeList(1));
+		
+		assertTrue(rd.getRhymeList(0).contains("blue"));
+    	assertTrue(rd.getRhymeList(0).contains("clue"));
+		assertTrue(rd.getRhymeList(0).contains("due"));
+		assertTrue(rd.getRhymeList(0).contains("shoe"));
+		assertTrue(rd.getRhymeList(0).contains("drew"));
+		
+		assertTrue(rd.getRhymeList(1).contains("black"));
+		assertTrue(rd.getRhymeList(1).contains("pac"));
+		assertTrue(rd.getRhymeList(1).contains("lack"));
+		
+		System.out.println(rd.toString());
+		
+	}
+	
+	@Test
+	public void testTwoSetMerged() {
+		RCRhymeDictionary rd = new RCRhymeDictionary();
+		
+		ArrayList<String> al = new ArrayList<String>();
+		
+		al.add("blue");
+		al.add("clue");
+		al.add("due");
+		
+		rd.addWords(al);
+		
+		ArrayList<String> al2 = new ArrayList<String>();
+		
+		al2.add("shoe");
+		al2.add("drew");
+		al2.add("blew");
+		
+		rd.addWords(al2);
+		
+        ArrayList<String> al3 = new ArrayList<String>();
+		
+		al3.add("blue");
+		al3.add("shoe");
+		al3.add("knew");
+		
+		rd.addWords(al3);
+		
+		System.out.println(rd.toString());
+		
+		
+		assertEquals(1,rd.getSize());
+		assertEquals(9,rd.getSizeGivenRhymeList(0));
+		
+		assertTrue(rd.getRhymeList(0).contains("blue"));
+    	assertTrue(rd.getRhymeList(0).contains("clue"));
+		assertTrue(rd.getRhymeList(0).contains("due"));
+		assertTrue(rd.getRhymeList(0).contains("shoe"));
+		assertTrue(rd.getRhymeList(0).contains("drew"));
+		assertTrue(rd.getRhymeList(0).contains("blew"));
+		assertTrue(rd.getRhymeList(0).contains("knew"));
+		
+	}
+	
+	@Test
+	public void testThreeSetMergedAndOneDifferent() {
+		RCRhymeDictionary rd = new RCRhymeDictionary();
+		
+		ArrayList<String> al = new ArrayList<String>();
+		
+		al.add("blue");
+		al.add("clue");
+		al.add("due");
+		
+		rd.addWords(al);
+		
+		ArrayList<String> al2 = new ArrayList<String>();
+		
+		al2.add("shoe");
+		al2.add("drew");
+		al2.add("blew");
+		
+		rd.addWords(al2);
+		
+        ArrayList<String> al3 = new ArrayList<String>();
+		
+		al3.add("black");
+		al3.add("rap");
+		al3.add("crack");
+		
+		rd.addWords(al3);
+		
+		ArrayList<String> al4 = new ArrayList<String>();
+
+		al4.add("true");
+		al4.add("shampoo");
+		al4.add("deja vu");
+
+		rd.addWords(al4);
+		
+		ArrayList<String> al5 = new ArrayList<String>();
+
+		al5.add("moo");
+		al5.add("blue");
+		al5.add("shoe");
+		al5.add("true");
+
+		rd.addWords(al5);
+
+		System.out.println(rd.toString());
+		
+		
+		assertEquals(2,rd.getSize());
+		assertEquals(13,rd.getSizeGivenRhymeList(0));
+		assertEquals(3,rd.getSizeGivenRhymeList(1));
+		
+		assertTrue(rd.getRhymeList(0).contains("blue"));
+    	assertTrue(rd.getRhymeList(0).contains("clue"));
+		assertTrue(rd.getRhymeList(0).contains("due"));
+		assertTrue(rd.getRhymeList(0).contains("shoe"));
+		assertTrue(rd.getRhymeList(0).contains("drew"));
+		assertTrue(rd.getRhymeList(0).contains("blew"));
+		assertTrue(rd.getRhymeList(0).contains("true"));
+		assertTrue(rd.getRhymeList(0).contains("shampoo"));
+		assertTrue(rd.getRhymeList(0).contains("deja vu"));
+		assertTrue(rd.getRhymeList(0).contains("moo"));
+		
+		assertTrue(rd.getRhymeList(1).contains("black"));
+		assertTrue(rd.getRhymeList(1).contains("rap"));
+		assertTrue(rd.getRhymeList(1).contains("crack"));
+		
+	}
+
 
 }
