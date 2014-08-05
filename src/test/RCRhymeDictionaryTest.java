8cdb173d7cbca6b8cc359e3107eca8c16af68577
Jordan_Wallet <jjwallet@email.arizona.edu>
2014-08-05 13:59:10 -0700

    start test

---
+++ src/test/RCRhymeDictionaryTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.util.ArrayList;
+
+import model.RCRhymeDictionary;
+
+import org.junit.Test;
+
+public class RCRhymeDictionaryTest {
+
+	@Test
+	public void testSingleSet() {
+		RCRhymeDictionary rd = new RCRhymeDictionary();
+		new ArrayList<String>();
+		
+		rd.addWords();
+	}
+
+}
