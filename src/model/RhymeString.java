2a913043bada3d4f9fcc92ec908d99d55bde5251
Jordan_Wallet <jjwallet@email.arizona.edu>
2014-08-07 13:55:14 -0700

    starting of making system aware of when a word can rhyme with itself

---
+++ src/model/RhymeString.java
@@ -0,0 +1,38 @@
+package model;
+
+/**
+ * RhymeString is a line ending that rhymes with other RhymeStrings.
+ *
+ */
+public class RhymeString {
+	private String value;
+	private boolean rhymesWithItself;
+	
+	public RhymeString(String value) {
+		this.value = value;
+		rhymesWithItself = false;
+	}
+	
+	public boolean rhymesWithItself() {
+		return rhymesWithItself;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+	
+	public void setRhymesWithItself(boolean rhymesWithItself) {
+		this.rhymesWithItself = rhymesWithItself;
+	}
+	
+	public int hashCode() {
+		return value.hashCode();
+	}
+	
+	public boolean equals(Object other) {
+		if(!(other instanceof RhymeString))
+			return false;
+		
+		return value.equals(((RhymeString) other).getValue());
+	}
+}
